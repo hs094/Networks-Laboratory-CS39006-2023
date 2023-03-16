e3e2ab61ae7ec05c365297c43b6e0173e10a1b8b
iamhardikat11 <iamhardikat11@gmail.com>
2023-03-16 19:01:56 +0530

    Make Changes in Ashutosh's File

--- Assignment 5/Assignment-4/19CS30008_A4/client.c
+++ Assignment 5/Assignment-4/19CS30008_A4/client.c
@@ -1,44 +1,69 @@
+
+/*    THE CLIENT PROCESS */
+
 #include "mysocket.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-#define PORT_1 50016
-#define MAX_MSG_LEN 5000
-// Client
+#define PORT 50017
 int main()
 {
-    int sockfd;
-    struct sockaddr_in serv_addr;
-    if ((sockfd = my_socket(AF_INET, SOCK_MyTCP, 0)) < 0)
-    {
-        perror("my_socket");
-        exit(1);
-    }
+	int			sockfd ;
+	struct sockaddr_in	serv_addr;
 
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(PORT_1);
+	int i;
+	char buf[100];
 
-    char msg[MAX_MSG_LEN];
-    while (1)
-    {
-        my_connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-        memset(msg, 0, MAX_MSG_LEN);
-        printf("Enter a message: ");
-        scanf("%[^\n]s", msg);   
-        int msg_len = strlen(msg);
-        // for (int i = 0; i < msg_len; i++)
-        // {
-        //     int ret = my_send(sockfd, &msg[i], 1, 0, (struct sockaddr *)&u2_addr, sizeof(u2_addr));
-        //     if (ret < 0)
-        //     {
-        //         perror("my_sendto");
-        //         exit(1);
-        //     }
-        // }
-    }
-    my_close(sockfd);
-    return 0;
+	/* Opening a socket is exactly similar to the server process */
+	if ((sockfd = my_socket(AF_INET, SOCK_MyTCP, 0)) < 0) {
+		perror("Unable to create socket\n");
+		exit(0);
+	}
+
+	/* Recall that we specified INADDR_ANY when we specified the server
+	   address in the server. Since the client can run on a different
+	   machine, we must specify the IP address of the server. 
+
+	   In this program, we assume that the server is running on the
+	   same machine as the client. 127.0.0.1 is a special address
+	   for "localhost" (this machine)
+	   
+	/* IF YOUR SERVER RUNS ON SOME OTHER MACHINE, YOU MUST CHANGE 
+           THE IP ADDRESS SPECIFIED BELOW TO THE IP ADDRESS OF THE 
+           MACHINE WHERE YOU ARE RUNNING THE SERVER. 
+    */
+
+	serv_addr.sin_family	= AF_INET;
+	inet_aton("127.0.0.1", &serv_addr.sin_addr);
+	serv_addr.sin_port	= htons(PORT);
+
+	/* With the information specified in serv_addr, the connect()
+	   system call establishes a connection with the server process.
+	*/
+	if ((my_connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) < 0) {
+		perror("Unable to connect to server\n");
+		exit(0);
+	}
+
+	/* After connection, the client can send or receive messages.
+	   However, please note that recv() will block when the
+	   server is not sending and vice versa. Similarly send() will
+	   block when the server is not receiving and vice versa. For
+	   non-blocking modes, refer to the online man pages.
+	*/
+	for(i=0; i < 100; i++) buf[i] = '\0';
+	my_recv(sockfd, buf, 100, 0);
+	printf("%s\n", buf);
+	strcpy(buf,"Message from client");
+	// send(sockfd, buf, strlen(buf) + 1, 0);
+	my_close(sockfd);
+	return 0;
+
 }
+
--- Assignment 5/Assignment-4/19CS30008_A4/mysocket.h
+++ Assignment 5/Assignment-4/19CS30008_A4/mysocket.h
@@ -14,12 +14,12 @@
 extern int tot_transm;
 
 int my_socket(int domain, int type, int protocol);
-int my_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+int my_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);;
 int my_listen(int sockfd, int k);
-int my_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
-int my_accept(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
-ssize_t my_send(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
-ssize_t my_recv(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen);
+int my_connect(int sockfd, struct sockaddr *serv_addr, socklen_t serv_len);
+int my_accept(int sockfd, struct sockaddr *cli_addr, socklen_t cli_len);
+ssize_t my_send(int sockfd, const char *buf, size_t len, int flags);
+ssize_t my_recv(int sockfd, char *buf, size_t len, int flags);
 int my_close(int fd);
 int dropMessage(float p);
 
